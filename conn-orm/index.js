214860c0c4d99d864c5b6bb0460dc18b3c07c52a
Debora Serra <debora.r.serra@gmail.com>
2024-11-22 13:37:09 -0800

    feat: add update without where example

--- conn-orm/index.js
+++ conn-orm/index.js
@@ -2,8 +2,18 @@ const http = require("http");
 const db = require("./models");
 
 const server = http.createServer(async (req, res) => {
+  if (req.url.includes("/update")) {
+    try {
+      const result = await db.Customer.update({ name: "John Doe" });
+      res.statusCode = 200;
+      res.end(JSON.stringify(result));
+    } catch (err) {
+      res.statusCode = 500;
+      res.end(err.message);
+    }
+  }
   const result = await db.Customer.findAll();
-  res.end(JSON.stringify(result))
+  res.end(JSON.stringify(result));
 });
 
 server.listen(3000, () => console.log("running on http://localhost:3000"));
